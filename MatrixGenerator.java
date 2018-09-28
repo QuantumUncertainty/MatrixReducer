ba2f6f3dadeb2b5fd9706012e31d43b6e98f1a64
Tucker <bullaratucker@gmail.com>
2018-09-28 09:03:33 -0500

    removed unnecessary code. Changed for loops to enhanced for loops.

--- MatrixGenerator.java
+++ MatrixGenerator.java
@@ -20,42 +20,30 @@ public class MatrixGenerator {
         System.out.println("Random or custom matrix? Enter R for random or C for custom: ");
         choice = reader.nextLine();
         
-        //discovered: bug is being caused by the for loop. Out of bounds exception for columns > rows
-        if(matrix.length != matrix[0].length){
-            if(choice.equals("R") || choice.equals("r")){
-                for(int i = 0; i < matrix.length; i++){
-                    for(int j = 0; j < matrix[0].length; j++){
-                        matrix[i][j] = (double) generator.nextInt(10) + 1;
-                    }
-                }
-            }
-            else if(choice.equals("C") || choice.equals("c")){
-                System.out.println("Enter desired entries into matrix: ");
-                for(int i = 0; i < matrix.length; i++){
-                    for(int j = 0; j < matrix[0].length; j++){
-                        matrix[i][j] = reader.nextInt();
-                    }          
+        Random generator = new Random();
+        Scanner reader = new Scanner(System.in);
+        String choice = "";
+               
+        System.out.println("Random or custom matrix? Enter R for random or C for custom: ");
+        choice = reader.nextLine();
+        
+        
+        if(choice.equals("R") || choice.equals("r")){
+            for (double[] matrix1 : matrix) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix1[j] = (double) generator.nextInt(10) + 1;
                 }
             }
         }
-        else{
-            if(choice.equals("R") || choice.equals("r")){
-                for(int i = 0; i < matrix.length; i++){
-                    for(int j = 0; j < matrix.length; j++){
-                        matrix[i][j] = (double) generator.nextInt(10) + 1;
-                    }
-                }
+        else if(choice.equals("C") || choice.equals("c")){
+            System.out.println("Enter desired entries into matrix: ");
+            for (double[] matrix1 : matrix) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix1[j] = reader.nextInt();
+                }          
             }
-            else if(choice.equals("C") || choice.equals("c")){
-                System.out.println("Enter desired entries into matrix: ");
-                for(int i = 0; i < matrix.length; i++){
-                    for(int j = 0; j < matrix.length; j++){
-                        matrix[i][j] = reader.nextInt();
-                    }          
-                }
-            }    
         }
-                    
+        
         System.out.println();
         System.out.println("Complete.");
         
