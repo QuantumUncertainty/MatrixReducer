81d18ff99b15d9237294277a7ad4c97dde738225
Tucker <bullaratucker@gmail.com>
2018-09-28 09:00:06 -0500

    deleted unnecessary code in printMatrix

--- MatrixReducer.java
+++ MatrixReducer.java
@@ -101,21 +101,11 @@ public class MatrixReducer{
     }
     
     public static void printMatrix(double[][] matrix){        
-        if(matrix.length != matrix[0].length){
-            for(int i = 0; i < matrix.length; i++){
-                for(int j = 0; j <= matrix[0].length; j++){
-                    System.out.print(matrix[i][j] + " ");                
-                }
-                System.out.println();
+        for (double[] matrix1 : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix1[j] + " ");                
             }
-        }
-        else{
-            for(int i = 0; i < matrix.length; i++){
-                for(int j = 0; j < matrix.length; j++){
-                    System.out.print(matrix[i][j] + " ");                
-                }
-                System.out.println();
-            }
+            System.out.println();
         }
               
         System.out.println();
