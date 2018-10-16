7c21822b308bc0c72a0e2c198c9b4e078f2cf6b0
Tucker <bullaratucker@gmail.com>
2018-10-16 17:37:28 -0500

    updated algorithms in test

--- MatrixAlgorithms.java
+++ MatrixAlgorithms.java
@@ -7,144 +7,76 @@ public class MatrixAlgorithms {
         double[][] matrix = {{6, 5, 1, 4},{4, 2, -1, 1},{5, 9, 3, -4},{4, 7, 8, 1}};
         MatrixOperator operator = new MatrixOperator(matrix, matrix.length, matrix[0].length);
                
-        int row = 0, column = 0;
-        double multiple = 0.0;
-        //more or less working, final column doesn't seem to be getting correct values
+        int row = 0, column = 0, pivotRow = 0;
+        double multiple;
+        
         while(row < matrix.length){
             if(matrix[row][column] == 0){
                 operator.swapRows(matrix, row, row + 1);
             }
-            else if(matrix[row][column] != 0 && matrix[row][column] != 1){
-                multiple = 1.0/matrix[row][column];
+            else if(matrix[row][column] != 0 && matrix[row][column] != 1){                
+                multiple = 1.0/matrix[row][column];                
                 operator.multiply(matrix, row, multiple);
+                pivotRow = row;
             }
             
-            operator.putZerosInRemainingEntries(matrix, column);                        
+            operator.putZerosInRemainingEntries(matrix, pivotRow);            
             column++;   
             row++;  
-        } 
-        
+        }
+
         return matrix;
     }
-    
-    //all tests following this comment need to be implemented
+
     public static double[][] toReducedRowEchelonFormTest2(){
-        double[][] matrix = {{}};
+        double[][] matrix = {{6, 5, 1, 4},{4, 2, -1, 1},{5, 9, 3, -4},{4, 7, 8, 1}};
         MatrixOperator operator = new MatrixOperator(matrix, matrix.length, matrix[0].length);
+               
+        int row = 0, column = 0, pivotRow = 0;
+        double multiple;
         
-        int row = 0, column = 0;
-        double multiple = 0.0;
-        //more or less working, final column doesn't seem to be getting correct values
         while(row < matrix.length){
             if(matrix[row][column] == 0){
                 operator.swapRows(matrix, row, row + 1);
             }
-            else if(matrix[row][column] != 0 && matrix[row][column] != 1){
-                multiple = 1.0/matrix[row][column];
+            else if(matrix[row][column] != 0 && matrix[row][column] != 1){                
+                multiple = 1.0/matrix[row][column];                
                 operator.multiply(matrix, row, multiple);
+                pivotRow = row;
             }
             
-            operator.putZerosInRemainingEntries(matrix, column);                        
+            operator.putZerosInRemainingEntries(matrix, pivotRow);            
             column++;   
             row++;  
         }
-        
+
         return matrix;
     }
-    
+
     public static double[][] toReducedRowEchelonFormTest3(){
-        double[][] matrix = {{}};
+        double[][] matrix = {{6, 5, 1, 4},{4, 2, -1, 1},{5, 9, 3, -4},{4, 7, 8, 1}};
         MatrixOperator operator = new MatrixOperator(matrix, matrix.length, matrix[0].length);
+               
+        int row = 0, column = 0, pivotRow = 0;
+        double multiple;
         
-        int row = 0, column = 0;
-        double multiple = 0.0;
-        //more or less working, final column doesn't seem to be getting correct values
         while(row < matrix.length){
             if(matrix[row][column] == 0){
                 operator.swapRows(matrix, row, row + 1);
             }
-            else if(matrix[row][column] != 0 && matrix[row][column] != 1){
-                multiple = 1.0/matrix[row][column];
+            else if(matrix[row][column] != 0 && matrix[row][column] != 1){                
+                multiple = 1.0/matrix[row][column];                
                 operator.multiply(matrix, row, multiple);
+                pivotRow = row;
             }
             
-            operator.putZerosInRemainingEntries(matrix, column);                        
+            operator.putZerosInRemainingEntries(matrix, pivotRow);            
             column++;   
             row++;  
         }
-        
+
         return matrix;
     }
-    
-    public static double[][] toReducedRowEchelonFormTest4(){
-        double[][] matrix = {{}};
-        MatrixOperator operator = new MatrixOperator(matrix, matrix.length, matrix[0].length);
-        
-        int row = 0, column = 0;
-        double multiple = 0.0;
-        //more or less working, final column doesn't seem to be getting correct values
-        while(row < matrix.length){
-            if(matrix[row][column] == 0){
-                operator.swapRows(matrix, row, row + 1);
-            }
-            else if(matrix[row][column] != 0 && matrix[row][column] != 1){
-                multiple = 1.0/matrix[row][column];
-                operator.multiply(matrix, row, multiple);
-            }
-            
-            operator.putZerosInRemainingEntries(matrix, column);                        
-            column++;   
-            row++;  
-        }
-        
-        return matrix;
-    }
-    
-    public static double[][] toReducedRowEchelonFormTest5(){
-        double[][] matrix = {{}};
-        MatrixOperator operator = new MatrixOperator(matrix, matrix.length, matrix[0].length);
-        
-        int row = 0, column = 0;
-        double multiple = 0.0;
-        //more or less working, final column doesn't seem to be getting correct values
-        while(row < matrix.length){
-            if(matrix[row][column] == 0){
-                operator.swapRows(matrix, row, row + 1);
-            }
-            else if(matrix[row][column] != 0 && matrix[row][column] != 1){
-                multiple = 1.0/matrix[row][column];
-                operator.multiply(matrix, row, multiple);
-            }
-            
-            operator.putZerosInRemainingEntries(matrix, column);                        
-            column++;   
-            row++;  
-        }
-        
-        return matrix;
-    }
-    
-    public static double[][] toReducedRowEchelonFormTest6(){
-        double[][] matrix = {{}};
-        MatrixOperator operator = new MatrixOperator(matrix, matrix.length, matrix[0].length);
-        
-        int row = 0, column = 0;
-        double multiple = 0.0;
-        //more or less working, final column doesn't seem to be getting correct values
-        while(row < matrix.length){
-            if(matrix[row][column] == 0){
-                operator.swapRows(matrix, row, row + 1);
-            }
-            else if(matrix[row][column] != 0 && matrix[row][column] != 1){
-                multiple = 1.0/matrix[row][column];
-                operator.multiply(matrix, row, multiple);
-            }
-            
-            operator.putZerosInRemainingEntries(matrix, column);                        
-            column++;   
-            row++;  
-        }
-        
-        return matrix;
-    }
+
+
 }
