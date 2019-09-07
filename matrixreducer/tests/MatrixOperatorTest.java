87db332b0c2059fbb538d7f63b3a5f5963d28add
tbullara <bullaratucker@gmail.com>
2019-09-07 09:39:26 -0500

    Adds test folder and test classes for MatrixOperator and MatrixReducer

---
+++ matrixreducer/tests/MatrixOperatorTest.java
@@ -0,0 +1,7 @@
+package matrixreducer.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixOperatorTest {
+
+}
---
+++ matrixreducer/tests/MatrixReducerTest.java
@@ -0,0 +1,49 @@
+package matrixreducer.tests;
+import matrixreducer.MatrixGenerator;
+import matrixreducer.MatrixReducer;
+import org.junit.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatrixReducerTest {
+
+    @org.junit.jupiter.api.Test
+    void toReducedRowEchelonForm() {
+        MatrixReducer[] reducers = new MatrixReducer[10];
+        for(int i = 0; i < reducers.length; i++){
+           //test matrices
+        }
+        //testing square matrices
+
+        //testing nonsquare matrices
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void toRowEchelonForm() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toUpperTriangular() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toLowerTriangular() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void swapRows() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addMultiple() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void putZerosInRemainingEntries() {
+    }
+}
