6fdd9aa025b7b0ac04ec2c76a3867744eef6d7cd
tbullara <bullaratucker@gmail.com>
2019-09-10 20:31:01 -0500

    added functions

--- matrixtools/interfaces/MatrixOperations.java
+++ matrixtools/interfaces/MatrixOperations.java
@@ -2,6 +2,8 @@ package matrixtools.interfaces;
 
 public interface MatrixOperations {
     public int determinant();
+    public int transpose();
+    public int inverse();
     public int norm();
     public int trace();
     //more operations coming
