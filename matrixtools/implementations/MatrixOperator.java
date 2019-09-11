e63fae1c46c4c82007290e57049cd58d799141cd
tbullara <bullaratucker@gmail.com>
2019-09-10 20:31:18 -0500

    implements methods in MatrixOperations interface

--- matrixtools/implementations/MatrixOperator.java
+++ matrixtools/implementations/MatrixOperator.java
@@ -1,5 +1,30 @@
 package matrixtools.implementations;
+import matrixtools.interfaces.MatrixOperations;
 
-public class MatrixOperator{   
+public class MatrixOperator implements MatrixOperations {
 
+    @Override
+    public int determinant() {
+        return 0;
+    }
+
+    @Override
+    public int transpose() {
+        return 0;
+    }
+
+    @Override
+    public int inverse() {
+        return 0;
+    }
+
+    @Override
+    public int norm() {
+        return 0;
+    }
+
+    @Override
+    public int trace() {
+        return 0;
+    }
 }
