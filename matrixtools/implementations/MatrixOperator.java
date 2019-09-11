2082385cae1c94258f1180f7f7284db76a06a87f
tbullara <bullaratucker@gmail.com>
2019-09-11 09:22:02 -0500

    implemented transpose function

--- matrixtools/implementations/MatrixOperator.java
+++ matrixtools/implementations/MatrixOperator.java
@@ -2,20 +2,36 @@ package matrixtools.implementations;
 import matrixtools.interfaces.MatrixOperations;
 
 public class MatrixOperator implements MatrixOperations {
+    private final double[][] matrix;
+    private final int rows;
+    private final int columns;
 
+    public MatrixOperator(double[][] matrix, int rows, int columns){
+        this.matrix = matrix;
+        this.rows = rows;
+        this.columns = columns;
+    }
     @Override
     public int determinant() {
         return 0;
     }
 
     @Override
-    public int transpose() {
-        return 0;
+    public double[][] transpose() {
+        double[][] transposed = new double[columns][rows];
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposed;
     }
 
     @Override
-    public int inverse() {
-        return 0;
+    public double[][] inverse() {
+        return null;
     }
 
     @Override
