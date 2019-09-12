184e2b8847712a96e346c5478befb1c14aac15c4
tbullara <bullaratucker@gmail.com>
2019-09-12 14:34:34 -0500

    added test file for MatrixOperator

--- matrixtools/tests/MatrixOperatorTest.java
+++ matrixtools/tests/MatrixOperatorTest.java
@@ -1,5 +1,33 @@
 package matrixtools.tests;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class MatrixOperatorTest {
 
+    @Test
+    void determinant() {
+    }
+
+    @Test
+    void transpose() {
+    }
+
+    @Test
+    void inverse() {
+    }
+
+    @Test
+    void matrixNorm() {
+    }
+
+    @Test
+    void lpNorm() {
+
+    }
+
+    @Test
+    void trace() {
+    }
 }
