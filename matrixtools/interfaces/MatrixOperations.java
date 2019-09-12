9b7a755cc384bdf88e18a75aa1452211c2c91147
tbullara <bullaratucker@gmail.com>
2019-09-12 14:35:02 -0500

    updated lpNorm definition

--- matrixtools/interfaces/MatrixOperations.java
+++ matrixtools/interfaces/MatrixOperations.java
@@ -5,7 +5,7 @@ public interface MatrixOperations {
     public double[][] transpose();
     public double[][] inverse();
     public double matrixNorm();
-    public double lpNorm();
+    public double lpNorm(int p);
     public double trace();
     //more operations coming
 }
